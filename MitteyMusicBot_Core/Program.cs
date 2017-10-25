7a174862107dbb26166d8e1681c6a486d0cf8c36
Mittey <d.k.kulikov@gmail.com>
2017-10-25 21:37:26 +0300

    Add project files.

---
+++ MitteyMusicBot_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MitteyMusicBot_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
